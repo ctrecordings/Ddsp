56ecaad1bc35a9dd2307bb4cd07b448176815022
Ethan Reker <ethanepr@hotmail.com>
2017-04-12 18:45:44 -0500

    Update functions.d

--- core/functions.d
+++ core/functions.d
@@ -1,3 +1,19 @@
 module dsp.core.functions;
 
-float floatToDecibel(
+import std.math;
+
+float floatToDecibel(float value){
+  return 20 * log(value);  
+}
+
+float dedibelToFloat(float value){
+  return pow(10, value/20);
+}
+
+float msToSamples(float ms, float sampleRate){
+  return ms * (sampleRate / 1000);
+}
+
+float samplesToMs(float samples, float sampleRate){
+  return samples / (1000 / sampleRate);
+}
