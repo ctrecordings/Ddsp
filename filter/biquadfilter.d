b5c125f6a0790110f670d73be283f82578878624
Ethan Reker <ethanepr@hotmail.com>
2017-04-12 20:49:54 -0500

    Update biquadfilter.d

--- filter/biquadfilter.d
+++ filter/biquadfilter.d
@@ -2,5 +2,25 @@ module dsp.filter.biquadfilter;
 
 class BiQuadFilter
 {
-
+  private import dsp.core.buffer;
+  
+  private int centerFrequency;
+  private float sampleRate;
+ 
+  private Buffer!float
+  this(int centerFrequency, double sampleRate)
+  {
+    this.centerFrequency = centerFrequency;
+    this.sampleRate = 
+  }
+  
+  float nextSample(float input)
+  {
+    
+  }
+  
+  virtual void update(float fc, float fs, float G = 0){}
+  
+  void setSampleRate(){ return this.sampleRate; }
+  float getSampleRate(){ this.sampleRate = sampleRate; }
 }
