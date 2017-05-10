36ba0cd7aaeab9d44b5cfc029b3aaf9bdfc1a5f2
abaga129 <ethanepr@hotmail.com>
2017-05-10 12:07:31 -0500

    add bufferIndex

--- core/buffer.d
+++ core/buffer.d
@@ -5,42 +5,66 @@ Purpose:  class to circular buffer data for use in delay lines and modulation.
 */
 module ddsp.core.buffer;
 
+/**
+Points to a specified location in a buffer.
+*/
+class bufferIndex
+{
+  static int numIndexes = 0;
+  size_t position;
+  int id;
+  this(string name, size_t position = 0)
+  {
+    id = ++numIndexes;
+    position = 0;
+  }
+}
+
 class Buffer (T)
 {
   T[] buffer;
   const default_size = 1000;
-  
+  bufferIndex[] indexes;
   size_t readIndex, writeIndex;
-  
+  size_t bufferLength;
+
   /+Initializes the Buffer with a set size +/
   this(size_t size)
   {
+    indexes ~= new bufferIndex("read");
+    indexes ~= new bufferIndex("write");
     buffer.length = size;
+    bufferLength = size;
     writeIndex = size - 1;
     readIndex = 0;
-	initialize();
+	  initialize();
   }
-  
+
   this()
   {
     buffer.length = default_size;
-	writeIndex = size - 1;
+	  writeIndex = size - 1;
     readIndex = 0;
-	initialize();
+	  initialize();
   }
-  
+
+  void addIndex(size_t position, string name)
+  {
+    indexes ~= new bufferIndex()
+  }
+
   void initialize(){
-	for(int i = 0; i < buffer.length; ++i)
-		buffer[i] = 0;
+  	for(int i = 0; i < buffer.length; ++i)
+  		buffer[i] = 0;
   }
-  
-  T read() nothrow @nogc 
+
+  T read() nothrow @nogc
   {
     T val = buffer[readIndex];
     increment(readIndex);
     return val;
   }
-  
+
   void write(T sample) nothrow @nogc
   {
     buffer[writeIndex] = sample;
@@ -48,51 +72,68 @@ class Buffer (T)
   }
   void resize(size_t size) nothrow @nogc
   {
+    bufferLength = size;
     size_t difference = buffer.length - size;
-    shiftReadIndex(difference);
+	  readIndex = (readIndex - difference) % size;
+    //shiftReadIndex(difference);
   }
-  
+
   void increment(ref size_t index) nothrow @nogc
   {
     if(++index == buffer.length)
       index = 0;
   }
-  
+
+  void increment(int indexId) nothrow @nogc
+  {
+    if(++index.position == buffer.length)
+      index.position = 0;
+  }
+
   void decrement(ref size_t index) nothrow @nogc
   {
     if(--index < 0)
       index = buffer.length - 1;
   }
-	
+
   void shiftReadIndex(ref size_t amount) nothrow @nogc
   {
-	  if(amount < 0){	  
-	  }
+	if(amount < 0){
+		for(int i = 0; i > amount; i--)
+			decrement(readIndex);
+	}
+	if(amount > 0){
+		for(int i = 0; i < amount; i++)
+			increment(readIndex);
+	}
   }
   size_t size() nothrow @nogc
   {
-	return buffer.length;
+	   return bufferLength;
   }
-  
+
   size_t rIndex() nothrow @nogc
   {
-	return readIndex;
+	   return readIndex;
   }
-  
+
   size_t wIndex() nothrow @nogc
   {
-	return writeIndex;
+	   return writeIndex;
   }
-  
+
 }
 
 @system unittest
-{	
+{
 	import std.stdio;
 	writeln("Buffer unittest..");
 	Buffer!float b = new Buffer!float(100);
 	for(int i = 0; i < 200; ++i){
 		b.write(i);
-		writefln("%s", b.read());
+		writef("%s ", b.read());
 	}
+  writefln("\nRead Index: %s\nShifting readIndex by 50...", b.rIndex());
+  b.resize(50);
+  writefln("Read Index: %s", b.rIndex());
 }
--- effect/delay.d
+++ effect/delay.d
@@ -13,7 +13,7 @@ class Delay
     this.mix = wet;
     buffer = new Buffer!float(delayAmount);
   }
-  
+
   float getNextSample(float sample) nothrow @nogc
   {
     float yn = buffer.read();
@@ -21,36 +21,43 @@ class Delay
     return mix * yn + sample * (1 - mix);
 	//return sample;
   }
-  
+
   /+This is a special case used in Entropy's crossover delay feature +/
   /*float getNextSample(float sample, float sideChain) nothrow @nogc
   {
     return 0;
   }*/
-  
-  void resize(size_t delayAmount) nothrow @nogc
+
+  void resize(size_t amount) nothrow @nogc
   {
-    buffer.resize(delayAmount);
+    buffer.resize(amount);
   }
-  
+
   void flush()
   {
-    
+
   }
-  
+
   size_t size() nothrow @nogc
   {
 	return buffer.size();
   }
-  
+
   void setFeedback(float fb) nothrow @nogc
   {
     feedback = fb;
   }
-  
+
   void setMix(float amount) nothrow @nogc
   {
     mix = amount;
+  }
+
+  size_t rIndex(){
+    return buffer.rIndex();
+  }
+  size_t wIndex(){
+    return buffer.wIndex();
   }
   private:
   Buffer!float buffer;
@@ -64,6 +71,9 @@ class Delay
 	import std.stdio;
 	Delay d = new Delay(100, 1, 1);
 	for(int i = 0; i < 200; ++i){
-		writefln("%s", d.getNextSample(i));
+		writef("%s ", d.getNextSample(i));
 	}
+  writefln("\nRead Index: %s Write Index: %s\nShifting readIndex by 27...\n", d.rIndex(), d.wIndex());
+  d.resize(27);
+  writefln("Read Index: %s Write Index: %s", d.rIndex(), d.wIndex());
 }
