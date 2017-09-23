084e64644ce650882f0b942f90fe111091773c10
abaga129 <ethanepr@hotmail.com>
2017-09-23 14:43:13 -0500

    rewrite lowshelf - it nolonger inherits from biquad
    Former-commit-id: 69ced074bf72a1839ce5adae0f96a56d402f3cc7

--- filter/ddsp/filter/shelf.d
+++ filter/ddsp/filter/shelf.d
@@ -1,31 +1,20 @@
+/**
+* Copyright 2017 Cut Through Recordings
+* License: MIT License
+* Author(s): Ethan Reker
+*/
 module ddsp.filter.shelf;
 
 import ddsp.filter.biquad;
+import ddsp.effect.aeffect;
 
 import std.math;
 
-class LowShelf : BiQuad
+class LowShelf : AEffect
 {
-nothrow:
-@nogc:
 public:
-    override void calcCoefficients()
-    {
-        _thetac = 2.0f * pi * _frequency / _sampleRate;
-        _mu = pow(10.0f, _gain / 20.0f);
-        _beta = 4.0f / (1.0f + _mu);
-        _delta = _beta * tan(_thetac / 2.0f);
-        _gamma = ( 1.0f - _delta) / (1.0f + _delta);
-        _a0 = (1.0f - _gamma) / 2.0f;
-        _a1 = _a0;
-        _a2 = 0.0f;
-        _b1 = - _gamma;
-        _b2 = 0.0f;
-        _c0 = (_mu - 1.0f);
-        _d0 = 1.0f;
-    }
 
-    void setGain(float gain)
+    void setGain(float gain) nothrow @nogc
     {
         if(_gain != gain)
         {
@@ -33,12 +22,104 @@ public:
             calcCoefficients();
         }
     }
+
+    override float getNextSample(float input)  nothrow @nogc
+    {
+        _w = input - _a1 * _w1 - _a2 * _w2;
+        _yn = _b0 * _w + _b1 *_w1 + _b2 * _w2;
+
+        _w2 = _w1;
+        _w1 = _w;
+
+        return _yn;
+    }
+
+    override void reset() nothrow @nogc
+    {
+        _w = 0;
+        _w1 = 0;
+        _w2 = 0;
+
+        _yn = 0;
+    }
+
+    void setFrequency(float frequency) nothrow @nogc
+    {
+        if(_frequency != frequency)
+        {
+            _frequency = frequency;
+            calcCoefficients();
+        }
+    }
+    
+    override string toString()
+    {
+        import std.conv;
+        return "a0: " ~ to!string(_a0) ~
+               "a1: " ~ to!string(_a1) ~
+               "a2: " ~ to!string(_a2) ~
+               "b0: " ~ to!string(_b0) ~
+               "b1: " ~ to!string(_b1) ~
+               "b2: " ~ to!string(_b2);
+    }
+
 private:
-    /// amount of gain in decibels
+    float _a0 = 0;
+    float _a1 = 0;
+    float _a2 = 0;
+    float _b0 = 0;
+    float _b1 = 0;
+    float _b2 = 0;
+
+    float _w = 0;
+    float _w1 = 0;
+    float _w2 = 0;
+
+    float _yn;
+
+    float _frequency;
     float _gain;
-    float _thetac;
-    float _mu;
-    float _beta;
-    float _delta;
-    float _gamma;
+
+    void calcCoefficients() nothrow @nogc
+    {
+        float shelfSlope = 2.0f;
+        float A  = pow (10, _gain/40);
+        float w0 = 2 * pi * _frequency / _sampleRate;
+        float cs = cos (w0);
+        float sn = sin (w0);
+        float AL = sn / 2 * sqrt ((A + 1/A) * (1 / shelfSlope - 1) + 2);
+        float sq = 2 * sqrt(A) * AL;
+        _b0 =    A*( (A+1) - (A-1)*cs + sq );
+        _b1 =  2*A*( (A-1) - (A+1)*cs );
+        _b2 =    A*( (A+1) - (A-1)*cs - sq );
+        _a0 =        (A+1) + (A-1)*cs + sq;
+        _a1 =   -2*( (A-1) + (A+1)*cs );
+        _a2 =        (A+1) + (A-1)*cs - sq;
+
+        _b0 /= _a0;
+        _b1 /= _a0;
+        _b2 /= _a0;
+        _a1 /= _a0;
+        _a2 /= _a0;
+    }
+}
+
+unittest
+{
+    import dplug.core.nogc;
+    import dplug.core.alignedbuffer;
+    import ddsp.effect.aeffect;
+
+    Vec!LowShelf filters = makeVec!LowShelf;
+    foreach(channel; 0..2)
+    {
+        filters.pushBack(mallocNew!LowShelf);
+        filters[channel].setSampleRate(44100.0f);
+        filters[channel].setFrequency(150.0f);
+        filters[channel].setGain(3.0f);
+    }
+
+    //testEffect(AEffect effect, string name, size_t bufferSize = 20000, bool outputResults = false)
+    foreach(filter; filters)
+        testEffect(filter, "LowShelf" , 20000, false);
 }
