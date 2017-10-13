36a27eacc78a013839ea97d5664f35cb02ccafa4
Ethan Reker <ethanepr@hotmail.com>
2017-10-12 20:23:23 -0500

    add sinc(x) and add documentation to functions.d
    Former-commit-id: 4ef8a13d38a5db4134bd9eea4c7178d27ae36394

--- util/ddsp/util/functions.d
+++ util/ddsp/util/functions.d
@@ -9,26 +9,48 @@ import std.math;
 import core.stdc.stdlib;
 import std.conv : emplace;
 
+/++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ + This is a collection of useful DSP related functions and algorithms.
+ + if you feel anything is missing, please feel free to add it.
+ +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++/
+
+/// Accepts a floating point value and returns its decibel equivalent.
+/// `value` should be in the range of -1 to 1 and returns a float in the
+/// range -96 to 0
 float floatToDecibel(float value) nothrow @nogc
 {
   return 20 * log(value);  
 }
 
-float dedibelToFloat(float value) nothrow @nogc
+/// Accepts a decibel value and returns its floating point equivalent.
+float decibelToFloat(float value) nothrow @nogc
 {
   return pow(10, value/20);
 }
 
+/// Accepts a time in milliseconds and the sample rate
+/// Returns the amount of samples that corresponds to the
+/// time in milliseconds.
 float msToSamples(float ms, float sampleRate) nothrow @nogc
 {
   return ms * (sampleRate / 1000);
 }
 
+/// Accepts a number of samples and the sample rate.
+/// Returns the corresponding time in milliseconds.
 float samplesToMs(float samples, float sampleRate) nothrow @nogc
 {
   return samples / (1000 / sampleRate);
 }
 
+/// Lagrange Interpolation
+/// Accepts:
+///     arrX = an array of x coordinates.
+///     arrY = an array of y coordinates.
+///     order = the number of coordinates (size of arrX & arrY)
+///     input = the x coordinate whose corresponding y value will be found
+/// Returns a y value along the curve that touches each (x,y) pair from arrX
+/// and arrY.  The pair (input, sum) is a point on that curve.
 float lagrpol(float[] arrX, float [] arrY, int order, float input) nothrow @nogc
 {
   float sum = 0;
@@ -45,9 +67,25 @@ float lagrpol(float[] arrX, float [] arrY, int order, float input) nothrow @nogc
   return sum;
 }
 
+/// Linear Interpolation between two points.
+/// (x1,y1) is the leftmost point and (x2,y2)
+/// is the rightmost point. x is some number
+/// between x1 and x2.
+/// Returns the corresopnding y value for x
+/// such that (x,y) is a point on the line
+/// that intersects (x1,y1) and (x2,y2)
 float linearInterp(float x1, float x2, float y1, float y2, float x) nothrow @nogc
 {
   return (x - x1) * (y2 - y1) / (x2 - x1) + y1;
+}
+
+/// sinc(x) aka the sampling function.
+float sinc(float x)
+{
+  if(x == 0)
+    return 1;
+  else
+    return sin(x) / x;
 }
 
 unittest
